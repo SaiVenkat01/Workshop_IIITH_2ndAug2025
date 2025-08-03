2dbfb70ef9314481f841ff2c76508bd64fc1d95c
Sai Venkat <saivenkat.k01@gmail.com>
2025-08-03 12:42:52 +0530

    workshop learnings

---
+++ practice_session/practice_1.py
@@ -0,0 +1,3 @@
+var1 = 'Your a customer support agent and will be doing sentimental analysis and will be identifying the user intention based on the message. You need to classify the message into categories such as questions, feedback, help, complaint.'
+
+
